798407741bf3f68c837ac55b2c5181e696b68dab
Jakob Klemm <github@jeykey.net>
2023-11-09 19:45:53 +0100

    Restart the data model.

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,44 @@
-use anyhow::Result;
+// Main
+
+use anyhow::Error;
+
+mod entry;
+mod processor;
+mod config;
+
+use config::Config;
+use entry::{Entry};
 use lazy_static::lazy_static;
-use polodb_core::Database;
-use std::env;
-
-// Application state logic
-pub mod application;
-
-// "Renderer"
-pub mod interface;
-
-// Event handler
-pub mod handler;
-use handler::Event;
-
-// Updater - Combine Events and App
-pub mod update;
-use update::update;
-
-// Abstract TUI functions
-pub mod terminal;
+use polodb_core::{Database, bson::doc};
 
 lazy_static! {
-    static ref DATABASE: Database = Database::open_file(format!(
-        "{}/dcp.db",
-        env::var("DCP_ROOT").unwrap_or(String::from("/database"))
-    ))
-    .unwrap();
+    pub static ref DATABASE: Database = {
+        let root = std::env::var("CCP_ROOT").unwrap_or("/database".to_string());
+        Database::open_file(format!("{}/ccp-polo.db", root)).expect("Unable to open database")
+    };
+    pub static ref ROOT: String = std::env::var("CCP_ROOT").unwrap_or("/database".to_string());
 }
 
-use ratatui::{backend::CrosstermBackend, Terminal as RatTerm};
-
-fn main() -> Result<()> {
-    let mut app = application::App::default();
-    let backend = CrosstermBackend::new(std::io::stderr());
-    let term = RatTerm::new(backend)?;
-    let events = handler::EventHandler::new(250);
-    let mut terminal = terminal::Terminal::new(term, events);
-
-    initialize_panic_handler();
-    terminal::startup()?;
-
-    while !app.terminate() {
-        terminal.draw(&mut app)?;
-        match terminal.events.next()? {
-            Event::Tick => {}
-            Event::Key(key_event) => update(&mut app, key_event),
-            Event::Mouse(_) => {}
-            Event::Resize(_, _) => {}
-        };
-    }
-
-    terminal::shutdown()?;
+fn main() -> Result<(), Error> {
+    ensure_configured();
+    let e = Entry::default();
+    println!("{:?}", e);
     Ok(())
 }
 
-pub fn initialize_panic_handler() {
-    let original_hook = std::panic::take_hook();
-    std::panic::set_hook(Box::new(move |panic_info| {
-        terminal::shutdown().unwrap();
-        original_hook(panic_info);
-    }));
+fn ensure_configured() {
+    let col = DATABASE.collection::<Config>("config");
+    let config = col.find_one(doc! {
+        "_id": "CONFIG"
+    }).expect("Unable to properly handle configuration.");
+    match config {
+        Some(c) => {
+            println!("Existing configuration found: {:?}", c);
+        }
+        None => {
+            println!("No existing configuration found, creating new one.");
+            let config = Config::default();
+            let _ = col.insert_one(config);
+        }
+    }
 }
